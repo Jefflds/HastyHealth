9ac04939c91abb91afb8df87e09f4ce27aa79edf
Jefflds <Jeffersonlds3009@gmail.com>
2024-01-26 16:32:20 -0300

    Resolvendo Lógica do Calculo de IMC e problema com tipagem

--- src/ui/components/Form/Form.tsx
+++ src/ui/components/Form/Form.tsx
@@ -3,8 +3,8 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import ResultImc from "../../partials/ResultImc/ResultImc";
 
 const Form: React.FC = () => {
-  const [height, setHeight] = useState<string>("");
-  const [weight, setWeight] = useState<string>("");
+  const [height, setHeight] = useState<number | null>(null);
+  const [weight, setWeight] = useState<number | null>(null);
   const [messageImc, setMessageImc] = useState<string>(
     "Preencha o Peso e a Altura"
   );
@@ -12,23 +12,24 @@ const Form: React.FC = () => {
   const [textButton, setTextButton] = useState<string>("Calcular");
 
   const ImcCalculator = (w: number, h: number) => {
-    const imcResult = w / (h * h);
-    setImc(imcResult);
-  };
-
-  const handleOnPress = () => {
-    try {
-      if (weight && height) {
-        ImcCalculator(Number(weight), Number(height));
-        setMessageImc(`Seu IMC é igual a: ${imc}`);
+    if (weight && height) {
+      if (h > 2) {
+        return setImc(w / ((h / 100) * (h / 100)));
       } else {
-        setMessageImc("Preencha o Peso e a Altura");
+        return setImc(w / (h * h));
       }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setTextButton("Calcular Novamente");
     }
+  };
+  const handleOnPress = () => {
+    setTextButton("Calculando...");
+    ImcCalculator(Number(weight), Number(height));
+    setMessageImc("Seu IMC é Igual A: ");
+    setTimeout(() => {
+      setTextButton("Calcular Novamente");
+    }, 3000);
+    setHeight(null);
+    setWeight(null);
+    setTextButton("Calcular");
   };
 
   return (
@@ -36,8 +37,8 @@ const Form: React.FC = () => {
       <View>
         <Text>Altura</Text>
         <TextInput
-          onChangeText={(text) => setHeight(text)}
-          value={height}
+          onChangeText={(e) => setHeight(Number(e))}
+          value={height?.toString()}
           placeholder="Ex. 1.75"
           keyboardType="numeric"
         />
@@ -45,8 +46,8 @@ const Form: React.FC = () => {
         <TextInput
           placeholder="Ex. 75.365"
           keyboardType="numeric"
-          onChangeText={(text) => setWeight(text)}
-          value={weight}
+          onChangeText={(e) => setWeight(Number(e))}
+          value={weight?.toString()}
         />
         <TouchableOpacity onPress={handleOnPress}>
           <View>
