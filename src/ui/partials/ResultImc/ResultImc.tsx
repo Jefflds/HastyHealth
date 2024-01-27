b41b3bda4c8e9359f31a69e05a26cf718e7ec849
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-27 00:14:58 -0300

    Criando uma funcao de compartilhar

--- src/ui/partials/ResultImc/ResultImc.tsx
+++ src/ui/partials/ResultImc/ResultImc.tsx
@@ -1,18 +1,33 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import { ResultImcStyles } from "../../styles/ResultImc/ResultImc.styles";
 
 type ResultImcProps = {
-    messageResultImc: string;
-    ResultImc: string | number | null | undefined;
-}
+  messageResultImc: string;
+  ResultImc: string | number | null | undefined;
+};
 
-const ResultImc: React.FC<ResultImcProps> = ({messageResultImc, ResultImc}) => {
+const ResultImc: React.FC<ResultImcProps> = ({
+  messageResultImc,
+  ResultImc,
+}) => {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Meu IMC é de ${ResultImc}`,
+    });
+  };
   return (
     <View style={ResultImcStyles.ResultImc}>
-        <Text style={ResultImcStyles.information}>{messageResultImc}</Text>
-        <Text style={ResultImcStyles.numberImc}>{ResultImc}</Text>
+      <View style={ResultImcStyles.boxShareButton}>
+        {ResultImc != null ? (
+          <TouchableOpacity onPress={onShare} style={ResultImcStyles.shared}>
+            <Text style={ResultImcStyles.sharedText}>Share</Text>
+          </TouchableOpacity>
+        ) : null}
+      </View>
+      <Text style={ResultImcStyles.information}>{messageResultImc}</Text>
+      <Text style={ResultImcStyles.numberImc}>{ResultImc}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ResultImc
+export default ResultImc;
